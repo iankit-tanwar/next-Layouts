b468a3d8d4ee1d40e0e12b70c30075c52969dd31
iankit-tanwar <tankitofficial01.gmail.com>
2023-07-14 11:41:47 +0530

    commit

---
+++ src/app/(layout1)/layout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style1.css'
+
+export default function layout({children}) {
+  return (
+    <div className='layout1'>{children}</div>
+  )
+}
---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function layout({children}) {
+  return (
+    <section id='Dashboard Layout'>{children}</section >
+  )
+}
